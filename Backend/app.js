26412091225ee8f8f0d7a7eff030591ae99627da
Usama <usama.mehmood2002@gmail.com>
2026-07-04 20:59:01 +0500

    Connect with mongoDB

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const connectToDb = require("./db/db");
+
+connectToDb();
+
 app.use(cors());
 
 app.get("/", (req, res) => {
