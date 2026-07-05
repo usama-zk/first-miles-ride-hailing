7c45b11c94152aeb3a38d42740e2ca2a9c366520
Usama <usama.mehmood2002@gmail.com>
2026-07-05 10:57:01 +0500

    feat(auth): add user registration capability

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,18 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
 const cors = require("cors");
 const app = express();
-
 const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Server Working ......");
 });
+
+app.use("/users", userRoutes);
 
 module.exports = app;
