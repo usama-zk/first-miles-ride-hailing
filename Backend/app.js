bed2bed6dee9fb6a86a78b3d7b35210f95420153
Usama <usama.mehmood2002@gmail.com>
2026-07-06 20:27:39 +0500

    feat(auth): USer authentication via cookie

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,14 @@
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 
 connectToDb();
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Server Working ......");
 });
