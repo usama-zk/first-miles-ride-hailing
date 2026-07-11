dd351fc975e4f20482151f6a8fe57f63f9c42db6
Usama <usama.mehmood2002@gmail.com>
2026-07-11 20:44:01 +0500

    feat: implement map service methods for distance, time, and autocomplete suggestions

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -10,5 +10,18 @@ router.get(
   authMiddleware.authUser,
   mapController.getCoordinates,
 );
+router.get(
+  "/get-distance-time",
+  query("origin").isString().isLength({ min: 3 }),
+  query("destination").isString().isLength({ min: 3 }),
+  authMiddleware.authUser,
+  mapController.getDistanceTime,
+);
 
+router.get(
+  "/get-suggestions",
+  query("input").isString().isLength({ min: 3 }),
+  authMiddleware.authUser,
+  mapController.getAutoCompleteSuggestions,
+);
 module.exports = router;
